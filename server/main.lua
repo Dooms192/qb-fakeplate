9e6289f4d8695cf4a2b5fca93faace7477324dea
Dooms192 <31485904+Dooms192@users.noreply.github.com>
2022-12-18 01:50:23 -0500

    Bug fix

--- server/main.lua
+++ server/main.lua
@@ -35,12 +35,13 @@ end)
 
 RegisterNetEvent('qb-fakeplate:server:removeFakePlate')
 AddEventHandler('qb-fakeplate:server:removeFakePlate', function(carPlate)
+    local src = source
     local check = MySQL.query.await('SELECT * FROM player_vehicles WHERE fakeplate = ? OR plate = ?', { carPlate, carPlate })
     local checkCarPlate
     for k, v in ipairs(check) do
         checkCarPlate = v['plate']
     end
-    TriggerClientEvent('qb-fakeplate:client:checkCarPlate', source, checkCarPlate, carPlate)
+    TriggerClientEvent('qb-fakeplate:client:checkCarPlate', src, checkCarPlate, carPlate)
     MySQL.Async.execute('UPDATE player_vehicles SET fakeplate = ? WHERE plate = ?', { nil , checkCarPlate })
 end)
 
